a2c131b9a01e8feac3ebcf9c2534d8a73269688a
Tarek <tarekmohamedd8@gmail.com>
2025-01-07 11:31:59 +0200

    Add re-capatcha to forget password

--- resources/views/front/auth/passwords/email.blade.php
+++ resources/views/front/auth/passwords/email.blade.php
@@ -42,6 +42,22 @@
                   <p class="text-danger mb-2 mt-2">{{ $message }}</p>
                 @enderror
               </div>
+
+              <div class="form_group">
+                                @if ($bs->is_recaptcha == 1)
+                                    <div class="d-block mb-4">
+                                        {!! NoCaptcha::renderJs() !!}
+                                        {!! NoCaptcha::display() !!}
+                                        @if ($errors->has('g-recaptcha-response'))
+                                            @php
+                                                $errmsg = $errors->first('g-recaptcha-response');
+                                            @endphp
+                                            <p class="text-danger mb-0 mt-2">{{ __("$errmsg") }}</p>
+                                        @endif
+                                    </div>
+                                @endif
+                </div>
+
               <div class="form-group">
                 <button class="btn btn-lg btn-primary ">{{ __('Send Password Reset Link') }}</button>
               </div>
