5daf42a5210a709ed761e9d3b38d452e9e69bc02
Tarek <tarekmohamedd8@gmail.com>
2025-03-17 10:27:50 +0200

    origins issue

--- config/cors.php
+++ config/cors.php
@@ -1,32 +1,8 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel CORS Options
-    |--------------------------------------------------------------------------
-    |
-    | The allowed_methods and allowed_headers options are case-insensitive.
-    |
-    | You don't need to provide both allowed_origins and allowed_origins_patterns.
-    | If one of the strings passed matches, it is considered a valid origin.
-    |
-    | If ['*'] is provided to allowed_methods, allowed_origins or allowed_headers
-    | all methods / origins / headers are allowed.
-    |
-    */
-
-    /*
-     * You can enable CORS for 1 or multiple paths.
-     * Example: ['api/*']
-     */
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Adjust for your Next.js frontend URL
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
