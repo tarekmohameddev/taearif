f12c8e769bc73a35e50302966c5774d4a81451fd
mn0unreal <mnfs2050@gmail.com>
2025-03-27 22:05:21 +0200

    Update base currency symbol
    Update base currency symbol and text to SAR

---
+++ database/migrations/2025_03_27_194322_update_base_currency_defaults_in_user_basic_settings.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_basic_settings', function (Blueprint $table) {
+            $table->string('base_currency_symbol')
+            ->default('https://upload.wikimedia.org/wikipedia/commons/9/98/Saudi_Riyal_Symbol.svg')
+            ->change();
+            $table->string('base_currency_text')
+            ->default('SAR')
+            ->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_basic_settings', function (Blueprint $table) {
+            //
+        });
+    }
+};
