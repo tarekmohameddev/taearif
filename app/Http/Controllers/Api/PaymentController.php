fa7be81e8f0fc8068cbc1b825262d49afc16eef7
mn0unreal <mnfs2050@gmail.com>
2025-05-13 11:23:05 +0300

    Fix PaymentController to use 'new_features' instead of 'features' for package data

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -85,9 +85,9 @@ class PaymentController extends Controller
                     'trial', 'is_trial' => 'تجريبي',
                     default => '',
                 },
-                'features' => is_array($package->features)
-                    ? $package->features
-                    : json_decode($package->features, true) ?? [],
+                'features' => is_array($package->new_features)
+                    ? $package->new_features
+                    : json_decode($package->new_features, true) ?? [],
                 'is_trial' => (bool) $package->is_trial,
                 'cta' => $isCurrent ? 'الخطة الحالية' :  'الترقية',
             ];
