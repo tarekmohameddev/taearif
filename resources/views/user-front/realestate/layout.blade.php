5e6a76b424eb43cf10d851af7875a4184bb258ac
mn0unreal <mnfs2050@gmail.com>
2025-05-20 20:15:50 +0300

    Add Google Tag Manager integration to layout

--- resources/views/user-front/realestate/layout.blade.php
+++ resources/views/user-front/realestate/layout.blade.php
@@ -100,6 +100,14 @@
         <!------google adsense----------->
     @endif
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KS62NNTG');</script>
+<!-- End Google Tag Manager -->
+
 <!-- Google tag (gtag.js) -->
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-RVFKM2F9ZN"></script>
@@ -119,6 +127,11 @@
 </head>
 
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KS62NNTG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <main>
 
