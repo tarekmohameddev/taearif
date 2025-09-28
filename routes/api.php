fbe924394fdcaa85b5b804a3a6908a24d7dde4ac
Tarek <tarekmohamedd8@gmail.com>
2025-09-28 18:12:32 +0300

    Tenant Website Properties API

--- routes/api.php
+++ routes/api.php
@@ -706,4 +706,8 @@ Route::prefix('v1/tenant-website')->middleware(['api','tenant.resolve'])->group(
     Route::put('{tenantId}/settings', [SettingsController::class, 'update'])->middleware('auth:sanctum');
     Route::post('{tenantId}/publish', [PublishController::class, 'store'])->middleware('auth:sanctum');
     Route::post('{tenantId}/forms/contact', [FormController::class, 'store']);
+
+    // Tenant Website Properties (public)
+    Route::get('{tenantId}/properties', [\App\Http\Controllers\Api\V1\TenantWebsite\PropertyController::class, 'index']);
+    Route::get('{tenantId}/properties/{slug}', [\App\Http\Controllers\Api\V1\TenantWebsite\PropertyController::class, 'show']);
 });
