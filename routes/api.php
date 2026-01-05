b94091d4e35e60c3cbdd7b510076f38169bd8214
mn0unreal <mnfs2050@gmail.com>
2026-01-05 03:51:13 +0200

    refactor: streamline property request routes by consolidating employee assignment endpoints

--- routes/api.php
+++ routes/api.php
@@ -691,12 +691,11 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
         Route::put('/property-requests/{id}/status', [ApiPropertyRequestController::class, 'updateStatus']);
         // update employee
         Route::put('/property-requests/{id}/employee', [ApiPropertyRequestController::class, 'updateEmployee']);
-    });
-
-    // Assign employee to customer via property request (direct customer update)
-    Route::prefix('v1')->middleware([SetTenantForPermissions::class, 'can:properties.view'])->group(function () {
+        // assign employee to customer
         Route::put('/property-requests/{customerID}/employee', [ApiPropertyRequestController::class, 'assignEmployeeToCustomer']);
     });
+
+
 
 
     // ApiPropertyRequestSettingsController
