9db0ad06071757811e16ca37452a2e946501a30b
mn0unreal <mnfs2050@gmail.com>
2026-01-19 22:49:57 +0200

    feat: Add event handling and data validation support to PropertiesTemplateExport for enhanced Excel export functionality

--- app/Exports/PropertiesTemplateExport.php
+++ app/Exports/PropertiesTemplateExport.php
@@ -10,6 +10,10 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\WithTitle;
 
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
+
 class PropertiesTemplateExport implements WithMultipleSheets
 {
     public function sheets(): array
@@ -21,9 +25,7 @@ class PropertiesTemplateExport implements WithMultipleSheets
     }
 }
 
-use Maatwebsite\Excel\Concerns\WithEvents;
-use Maatwebsite\Excel\Events\AfterSheet;
-use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
+
 
 class PropertiesTemplateMainSheetExport implements FromArray, WithHeadings, WithTitle, WithEvents
 {
